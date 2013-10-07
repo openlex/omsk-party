442099a3163f0850d1525b339f85deb20d8afb82
Alexey <openlexx@gmail.com>
2013-10-08 01:22:55 +0700

    photo grid

--- script/sliding-comment-block.js
+++ script/sliding-comment-block.js
@@ -1,9 +1,19 @@
 $(document).ready(function() { 
 	var reply=false;
+	$(".reply").css("margin-top", '-150px');
+	$(".reply").css("display", "none");
+	$(".reply_textarea").css("visibility", "hidden");
+	
 	$(".js_answer",this).click(function() {
+		$(this).parent().next().animate({"margin-top":"-26px"},700);
+		//$(".reply").css("margin-top", '-26px');
+		$(this).parent().next().css("display", "block");
 
-		//$(".reply").css("display", "block");
-		//$(".reply").animate({fontSize:"1.3em"},200);
+		$(".reply").mouseenter(function() {
+      		$(".reply_textarea",this).css("visibility", "visible");
+    	});
+		
+		
 
 		/*if (reply==false){
 			reply=!reply;
@@ -12,6 +22,15 @@ $(document).ready(function() {
 
 		}*/
 	});
+
+	$(".send_photo_reply_button").click(function() {
+		$(this).parent().next().animate({"margin-top":"-150px"},700);
+		
+		setTimeout( function(){
+			$(this).parent().next().css("display", "block");
+		 },700);
+		
+	})
 })
 
 
